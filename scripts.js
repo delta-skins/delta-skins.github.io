f66330dffe3ae63f57f3a212338b0e1c75e6279e
jimmyshadow1 <jimmyshadow0@gmail.com>
2019-10-07 18:17:33 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -85,3 +85,15 @@ function includeLandscape(element){
     }
   }
 }
+var eventHandler = function (event) {
+    // Only run for iOS full screen apps
+    if (('standalone' in window.navigator) && window.navigator.standalone) {
+        // Only run if link is an anchor and points to the current page
+        if ( event.target.tagName.toLowerCase() !== 'a' || event.target.hostname !== window.location.hostname || event.target.pathname !== window.location.pathname || !/#/.test(event.target.href) ) return;
+
+        // Open link in same tab
+        event.preventDefault();
+        window.location = event.target.href;
+    }
+}
+window.addEventListener('click', eventHandler, false);
