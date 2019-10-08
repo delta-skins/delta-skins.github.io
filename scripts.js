e6b6a3f77bc2af249770f8fad3d2d5d051ccafc9
Jimmy <jimmyshadow0@gmail.com>
2019-10-07 23:48:00 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -85,15 +85,55 @@ function includeLandscape(element){
     }
   }
 }
-var eventHandler = function (event) {
-    // Only run for iOS full screen apps
-    if (('standalone' in window.navigator) && window.navigator.standalone) {
-        // Only run if link is an anchor and points to the current page
-        if ( event.target.tagName.toLowerCase() !== 'a' || event.target.hostname !== window.location.hostname || event.target.pathname !== window.location.pathname || !/#/.test(event.target.href) ) return;
-
-        // Open link in same tab
-        event.preventDefault();
-        window.location = event.target.href;
-    }
-}
-window.addEventListener('click', eventHandler, false);
+if (typeof jQuery != 'undefined') {
+    jQuery(document).ready(function($) {
+      var filetypes = /\.(zip|exe|dmg|pdf|doc.*|xls.*|ppt.*|mp3|txt|rar|wma|mov|avi|wmv|flv|wav|deltaskin)$/i;
+      var baseHref = '';
+      if (jQuery('base').attr('href') != undefined) baseHref = jQuery('base').attr('href');
+  
+      jQuery('a').on('click', function(event) {
+        var el = jQuery(this);
+        var track = true;
+        var href = (typeof(el.attr('href')) != 'undefined' ) ? el.attr('href') :"";
+        var isThisDomain = href.match(document.domain.split('.').reverse()[1] + '.' + document.domain.split('.').reverse()[0]);
+        if (!href.match(/^javascript:/i)) {
+          var elEv = []; elEv.value=0, elEv.non_i=false;
+          if (href.match(/^mailto\:/i)) {
+            elEv.category = "email";
+            elEv.action = "click";
+            elEv.label = href.replace(/^mailto\:/i, '');
+            elEv.loc = href;
+          }
+          else if (href.match(filetypes)) {
+            var extension = (/[.]/.exec(href)) ? /[^.]+$/.exec(href) : undefined;
+            elEv.category = "download";
+            elEv.action = "click-" + extension[0];
+            elEv.label = href.replace(/ /g,"-");
+            elEv.loc = baseHref + href;
+          }
+          else if (href.match(/^https?\:/i) && !isThisDomain) {
+            elEv.category = "external";
+            elEv.action = "click";
+            elEv.label = href.replace(/^https?\:\/\//i, '');
+            elEv.non_i = true;
+            elEv.loc = href;
+          }
+          else if (href.match(/^tel\:/i)) {
+            elEv.category = "telephone";
+            elEv.action = "click";
+            elEv.label = href.replace(/^tel\:/i, '');
+            elEv.loc = href;
+          }
+          else track = false;
+  
+             if (track) {
+            _gaq.push(['_trackEvent', elEv.category.toLowerCase(), elEv.action.toLowerCase(), elEv.label.toLowerCase(), elEv.value, elEv.non_i]);
+            if ( el.attr('target') == undefined || el.attr('target').toLowerCase() != '_blank') {
+              setTimeout(function() { location.href = elEv.loc; }, 400);
+              return false;
+        }
+      }
+        }
+      });
+    });
+  }
